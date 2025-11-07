075df55cf38bd0489b870d5a20e88c5e05eff05c
adamkocsis <adam.t.kocsis@gmail.com>
2025-11-07 11:28:28 +0100

    presence_kent

--- NEWS.md
+++ NEWS.md
@@ -1,6 +1,12 @@
 # Change log of the R package 'biodome'
 
-# biodome 0.1.0-1 - 2025-06-25
+# biodome 0.2.0-1 - 2025-11-07
+
+## Changed
+
+- renamed the `kentPresence` function to `presence_kent` and changed the `drop` argument to `listout`
+
+# biodome 0.1.0 - 2025-06-25
 
 ### Added
 - Initial build. Some fake data generators and range estimators.
--- R/points.R
+++ R/points.R
@@ -6,10 +6,18 @@
 #' @param kappa A vector of kappa parameters of the Kent distribution. kappa = a indicates a uniform distribution, the higher kappa is, the concentrated the presences.
 #' @param centers The center points around which the points are to created (matrix). If \code{NULL}, then these will be randomized.
 #' @param beta The ovalness parameter of the Kent distribution
-#' @param drop If there is a single distribution to be created, should this ve a single matrix, rather than a list with one element?
-#' @return A list with with as many elements, as the length of n. Each element will be a matrix of point coordinates.
+#' @param listout For multiple distributions, should the outputs be separated (list) or returned in a single matrix?
+#' @return A two-column matrix of point coordinates or a list of matrices.
+#' @examples
+#' # uniform points on a sphere
+#' uniform <- presence_kent(n=400, kappa=0)
+#' # points around the 50 lon, 80 lat point
+#' conc <- presence_kent(n=100, kappa=50, centers=matrix(c(50,80), ncol=2))
+#' plot(NULL, NULL, xlim=c(-180, 180), ylim=c(-90,90))
+#' points(uniform, col="red", pch=1)
+#' points(conc, col="blue", pch=3)
 #' @export
-kentPresence <- function(n, kappa, centers=NULL, beta=0, drop=TRUE){
+presence_kent <- function(n, kappa, centers=NULL, beta=0, listout=FALSE){
 	if(!requireNamespace("Directional", quietly=TRUE)) stop("This function requires the 'Directional' package.")
 
 
@@ -27,22 +35,37 @@ kentPresence <- function(n, kappa, centers=NULL, beta=0, drop=TRUE){
 	# the centers on a unit sphere
 	unitCenters <- icosa::PolToCar(centers, radius=1)
 
-	# generate as man
-	pointSet <- list()
-
+	if(!listout){
+		pointSet<- matrix(NA, ncol=2, nrow=sum(n))
+		cumN <- cumsum(n)
+	}else{
+		# generate as man
+		pointSet <- list()
+	}
+	
 	for(i in 1:s){
 
-		# points along a great circle
-		unitPoints <- Directional::rkent(n=n[i], k=kappa[i], m=as.numeric(unitCenters[i,,drop=FALSE]), b=beta)
+		#this shit is bugy
+		go <- TRUE
+		# try to do this.... iteratively, gives an error with some random numbers
+		while(go){
+			try({
+				# points along a great circle
+				unitPoints <- Directional::rkent(n=n[i], k=kappa[i], m=as.numeric(unitCenters[i,,drop=FALSE]), b=beta)
+				go <- FALSE
+			}, silent=TRUE)
+		}
 
-		# back to longitude and latitude
-		pointSet[[i]]  <- icosa::CarToPol(unitPoints)[,c(1,2)]
+		if(listout){
+			# back to longitude and latitude
+			pointSet[[i]]  <- icosa::CarToPol(unitPoints)[,c(1,2)]
+		}else{
+			if(i==1) offset <- 0 else offset <- cumN[i-1]
+			pointSet[1:n[i]+offset,]  <- icosa::CarToPol(unitPoints)[,c(1,2)]
+
+		}
 	}
 
-	if(s==1 & drop){
-		return(pointSet[[1]])
-	}else{
-		return(pointSet)
-	}
+	return(pointSet)
 
 }
